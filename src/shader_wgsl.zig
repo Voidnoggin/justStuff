fcdfe0f4af49d199a264a8c5070167025f4befd2
Robin Cochrane <o0theusername0o@gmail.com>
2024-08-26 11:46:19 -0600

    changed redundant things in shader

--- src/shader_wgsl.zig
+++ src/shader_wgsl.zig
@@ -26,7 +26,7 @@ pub const basic = struct {
     \\      var output: VertexOut;
     \\      output.position_clip = vec4(position, 1.0) * draw_uniforms.object_to_world * frame_uniforms.world_to_clip;
     \\      output.position = (vec4(position, 1.0) * draw_uniforms.object_to_world).xyz;
-    \\      output.normal = (vec4(normal, 0.0) * draw_uniforms.object_to_world).xyz;
+    \\      output.normal = vec4(normal, 0.0).xyz;
 	\\		output.texcoord = vec4(texcoord, 0.0, 0.0).xy;
     \\      return output;
     \\  }
@@ -37,7 +37,7 @@ pub const basic = struct {
     \\      @location(1) normal: vec3<f32>,
 	\\		@location(2) texcoord: vec2<f32>,
     \\  ) -> @location(0) vec4<f32> {
-    \\      return vec4(texcoord.xy, 1.0, 1.0);
+    \\      return vec4(normal.xyz, 1.0);
     \\  }
     ;
 };
