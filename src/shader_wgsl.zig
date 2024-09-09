716588b48e4303c5ae005f9c2b634f13596d6d21
Robin Cochrane <o0theusername0o@gmail.com>
2024-09-09 16:31:21 -0600

    displays avocado texture now

--- src/shader_wgsl.zig
+++ src/shader_wgsl.zig
@@ -16,28 +16,30 @@ pub const basic = struct {
     \\      @builtin(position) position_clip: vec4<f32>,
     \\      @location(0) position: vec3<f32>,
     \\      @location(1) normal: vec3<f32>,
-	\\		@location(2) texcoord: vec2<f32>,
+    \\        @location(2) texcoord: vec2<f32>,
     \\  }
     \\  @vertex fn main(
     \\      @location(0) position: vec3<f32>,
     \\      @location(1) normal: vec3<f32>,
-	\\		@location(2) texcoord: vec2<f32>,
+    \\        @location(2) texcoord: vec2<f32>,
     \\  ) -> VertexOut {
     \\      var output: VertexOut;
     \\      output.position_clip = vec4(position, 1.0) * draw_uniforms.object_to_world * frame_uniforms.world_to_clip;
     \\      output.position = (vec4(position, 1.0) * draw_uniforms.object_to_world).xyz;
     \\      output.normal = vec4(normal, 0.0).xyz;
-	\\		output.texcoord = vec4(texcoord, 0.0, 0.0).xy;
+    \\        output.texcoord = vec4(texcoord, 0.0, 0.0).xy;
     \\      return output;
     \\  }
     ;
     pub const fs = common ++
+    \\  @group(0) @binding(1) var base_map: texture_2d<f32>;
+    \\  @group(0) @binding(2) var texture_sampler: sampler;
     \\  @fragment fn main(
     \\      @location(0) position: vec3<f32>,
     \\      @location(1) normal: vec3<f32>,
-	\\		@location(2) texcoord: vec2<f32>,
+    \\        @location(2) texcoord: vec2<f32>,
     \\  ) -> @location(0) vec4<f32> {
-    \\      return vec4(normal.xyz, 1.0);
+    \\      return textureSample(base_map, texture_sampler, texcoord,);
     \\  }
     ;
 };
